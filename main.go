18fc95c2f9c1cf15ee68ec8a814d4cbc7c8fa1d3
Elian Avalos <elianemanuelstudy@gmail.com>
2023-03-25 08:53:41 -0300

    Check if the request path match the index route
    If it does'nt match send a not found text

--- main.go
+++ main.go
@@ -6,15 +6,22 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path exactly matches "/". If it doesn't, use
+	// the http.NotFound() function to send a 404 response to the client.
+	// Importantly, we then return from the handler. If we don't return the handler
+	// would keep executing and also write the "Hello from SnippetBox" message.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-// Add a showSnippet handler function
 func showSnippet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet..."))
 }
 
-// Add a createSnippet handler function
 func createSnippet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Create a new snippet..."))
 }
@@ -22,8 +29,6 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	// Register the two new handler functions and corresponding URL patterns with
-	// the servemux, in exactly the same way that we did before.
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
