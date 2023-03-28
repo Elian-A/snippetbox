7f0bb1cef1b4ae29828500269519ccf2ad386d22
Elian Avalos <elianemanuelstudy@gmail.com>
2023-03-28 05:53:03 -0400

    Add centralized error handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,11 +7,9 @@ import (
 	"strconv"
 )
 
-// Change the signature of the home handler so it is defined as a method against
-// *application.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w) // Use the notFound() helper
 		return
 	}
 	files := []string{
@@ -22,35 +20,29 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, err) // Use the serverError() helper
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, err) // Use the serverError() helper
 	}
 }
 
-// Change the signature of the home handler so it is defined as a method against
-// *application.
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w) // Use the notFound helper
 		return
 	}
 	fmt.Fprintf(w, "Display an specific snippet with ID %d...", id)
 }
 
-// Change the signature of the home handler so it is defined as a method against
-// *application.
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed) // Use the clientError() helper
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
