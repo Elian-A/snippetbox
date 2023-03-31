41af64c24bfc2630bdbe974cdacb13cb0f39eed8
Elian Avalos <elianemanuelstudy@gmail.com>
2023-03-31 10:35:41 -0400

    Add a transaction example

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -102,3 +102,44 @@ func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
 	// If everything went OK then return the Snippets slice.
 	return snippets, nil
 }
+
+/*
+type ExampleModel struct {
+    DB *sql.DB
+}
+func (m *ExampleModel) ExampleTransaction() error {
+    // Calling the Begin() method on the connection pool creates a new sql.Tx
+    // object, which represents the in-progress database transaction.
+    tx, err := m.DB.Begin()
+    if err != nil {
+        return err
+    }
+    // Call Exec() on the transaction, passing in your statement and any
+    // parameters. It's important to notice that tx.Exec() is called on the
+    // transaction object just created, NOT the connection pool. Although we're
+    // using tx.Exec() here you can also use tx.Query() and tx.QueryRow() in
+    // exactly the same way.
+    _, err = tx.Exec("INSERT INTO ...")
+    if err != nil {
+        // If there is any error, we call the tx.Rollback() method on the
+        // transaction. This will abort the transaction and no changes will be
+        // made to the database.
+        tx.Rollback()
+        return err
+    }
+    // Carry out another transaction in exactly the same way.
+    _, err = tx.Exec("UPDATE ...")
+    if err != nil {
+        tx.Rollback()
+        return err
+    }
+    // If there are no errors, the statements in the transaction can be committed
+    // to the database with the tx.Commit() method. It's really important to ALWAYS
+    // call either Rollback() or Commit() before your function returns. If you
+    // don't the connection will stay open and not be returned to the connection
+    // pool. This can lead to hitting your maximum connection limit/running out of
+    // resources.
+    err = tx.Commit()
+    return err
+}
+*/
